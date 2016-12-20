72b45505cf54660581b234ae440c5f8d1ac489eb
edenadler <eden.n.adler@gmail.com>
2016-12-20 12:12:01 +0200

    solution using CANVAS properties, but will change format entirely after this

--- Paint95Assignment.js
+++ Paint95Assignment.js
@@ -1,7 +1,7 @@
-var colorChoice = "black";
 
+//create drawing studio
 function init(){
-var canvasDiv = document.createElement("DIV");
+var canvasDiv = document.createElement("CANVAS");
 document.body.appendChild(canvasDiv);
 canvasDiv.setAttribute("id","canvas");
 
@@ -14,39 +14,38 @@ var colorMenuDiv = document.createElement("DIV");
 colorMenuDiv.setAttribute("id", "colorMenu");
 menuDiv.appendChild(colorMenuDiv);
 
-var colorRedDiv = document.createElement("DIV");
+var colorRedDiv = document.createElement("BUTTON");
 colorRedDiv.setAttribute("id","colorRed");
 colorRedDiv.setAttribute("class","colors");
 colorMenuDiv.appendChild(colorRedDiv);
 
-var colorBlueDiv = document.createElement("DIV");
+var colorBlueDiv = document.createElement("BUTTON");
 colorBlueDiv.setAttribute("id","colorBlue");
 colorRedDiv.setAttribute("class","colors");
 colorMenuDiv.appendChild(colorBlueDiv);
 
-var colorGreenDiv = document.createElement("DIV");
+var colorGreenDiv = document.createElement("BUTTON");
 colorGreenDiv.setAttribute("id","colorGreen");
 colorRedDiv.setAttribute("class","colors");
 colorMenuDiv.appendChild(colorGreenDiv);
 
-var colorOrangeDiv = document.createElement("DIV");
+var colorOrangeDiv = document.createElement("BUTTON");
 colorOrangeDiv.setAttribute("id","colorOrange");
 colorRedDiv.setAttribute("class","colors");
 colorMenuDiv.appendChild(colorOrangeDiv);
 
-var colorPurpleDiv = document.createElement("DIV");
+var colorPurpleDiv = document.createElement("BUTTON");
 colorPurpleDiv.setAttribute("id","colorPurple");
 colorRedDiv.setAttribute("class","colors");
 colorMenuDiv.appendChild(colorPurpleDiv);
 
-var allColors = document.getElementsByClassName("colors");
-    for (var i = 0; i < allColors.length; i++){
-        allColors[i].addEventListener("click",selectColor);
-    }
-var colorCanvas = document.getElementById("canvas");
-colorCanvas.addEventListener("click",drawColor);
-} //init function close
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+var pos = { x: 0, y: 0};
+var paint = false;
+var colorChoice = "black";
 
+//Choosing the color
 function selectColor(e){
 	colorChoice = e.target.backgroundColor;
 }
@@ -57,17 +56,40 @@ function drawColor(e){
         alert("please choose a color first!");
         return;
     }
- //    var newSticker = document.createElement("img");
- //    newSticker.src = "./images/" + selectedSticker + ".png";
- //    newSticker.classList.add("sticker");
- //    stickersCanvas.appendChild(newSticker);
- //    var x = e.pageX;
- //    var y = e.pageY;
-	// console.log(x,y)
- //    x -= stickersCanvas.offsetLeft + 50;
- //    y -= stickersCanvas.offsetTop + 50;
- //    newSticker.style.left = x +'px';
- //    newSticker.style.top = y +'px';
- //WORKING ON HOW TO DRAW THE COLOR ONCE IT IS SELECTED
 }
+
+//DRAWING
+function mouseDown(e){
+	pos.x = e.pageX-this.offsetLeft;
+    pos.y = e.pageY-this.offsetTop;
+    ctx.beginPath();
+    ctx.moveTo(pos.x,pos.y);
+    paint = true;
+}
+
+function mouseMove(e){
+	if (paint){
+		ctx.lineTo(pos.x,pos.y);
+		ctx.strokeStyle = colorChoice;
+		ctx.stroke();
+	}
+}
+
+function mouseUp(e){
+	paint = false;
+}
+
+var allColors = document.getElementsByClassName("colors");
+    for (var i = 0; i < allColors.length; i++){
+        allColors[i].addEventListener("click",selectColor);
+    }
+var colorCanvas = document.getElementById("canvas");
+colorCanvas.addEventListener("click",drawColor);
+
+//DRAWING - EVENT LISTENER
+document.getElementById("canvas").addEventListener("mousedown",mouseDown);
+document.getElementById("canvas").addEventListener("mousemove",mouseMove);
+document.getElementById("canvas").addEventListener("mouseup",mouseUp);
+
+} //init function close
 
