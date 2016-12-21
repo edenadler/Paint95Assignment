e4a578bef1d5d1c314938858e9b8c6614f1d1080
edenadler <eden.n.adler@gmail.com>
2016-12-21 13:01:47 +0200

    changing background color of divs, but happens very slowly

--- Paint95Assignment.js
+++ Paint95Assignment.js
@@ -4,6 +4,7 @@ function init(){
 var canvasDiv = document.createElement("DIV");
 document.body.appendChild(canvasDiv);
 canvasDiv.setAttribute("id","canvas");
+document.getElementById("canvas").style.overflowY= "hidden";
 
 var menuDiv = document.createElement("DIV");
 document.body.appendChild(menuDiv);
@@ -49,23 +50,51 @@ var allColors = document.getElementsByClassName("colors");
 		allColors[i].addEventListener("click",selectColor);
 	}
 
-document.getElementById("canvas").addEventListener("mousedown", setPosition);
-document.getElementById("canvas").addEventListener("mousemove", draw);
-document.getElementById("canvas").addEventListener("mouseup", finishDrawing);
+// document.getElementById("canvas").addEventListener("mousedown", setPosition);
+// document.getElementById("canvas").addEventListener("mousemove", draw);
+// document.getElementById("canvas").addEventListener("mouseup", finishDrawing);
+
+
+//making event listener for each pixel in the canvas
+	var mainCanvas = document.getElementById("canvas");
+	var j = 0;
+	for (var i = 0; i<500; i++){
+		j = 0;
+		while (j<500) {
+			var pixel = document.createElement("DIV");
+			mainCanvas.appendChild(pixel);
+			pixel.setAttribute("class","pixels");
+			pixel.style.height = "1px";
+			pixel.style.width = "1px";
+			pixel.style.display = "inline-block";
+			pixel.style.position = "absolute";
+			pixel.style.top = i + "px";
+			pixel.style.left = j + "px";
+			j++;
+		}
+	}
+	var allPixels = document.getElementsByClassName("pixels");
+	for (var i = 0; i<allPixels.length; i++){
+		allPixels[i].addEventListener("mousedown", setPosition);
+		allPixels[i].addEventListener("mouseover", draw);
+		allPixels[i].addEventListener("mouseup", finishDrawing);
+	}	
 
 } //init function close
+
 var paint = false;
 var colorChoice = "black";
-var pos;
+var mouse = {x: "", y: ""};
+
 
 //Choosing the color
 function selectColor(e){
 	colorChoice = e.target.style.backgroundColor;
-	paint = true;
 }
 
 function setPosition(e){
-	pos = {x: e.pageX , y: e.pageY};
+	mouse = {x: e.pageX , y: e.pageY};
+	paint = true;
 }
 
 function draw(e){
@@ -74,18 +103,9 @@ function draw(e){
         alert("please choose a color first!");
         return;
 	}
-	var newPixel = document.createElement("DIV");
-	newPixel.setAttribute("class","pixels");
-	newPixel.setAttribute("id","pixel");
-	newPixel.setAttribute("style","height: 5px; width: 5px; display: block; position: absolute;");
-	newPixel.style.left = pos.x +'px';
-  	newPixel.style.top = pos.y +'px';
-    newPixel.style.backgroundColor = colorChoice;
-	canvasSpace.appendChild(newPixel);
-	pos = {x: e.pageX , y: e.pageY};
-    console.log(pos.x,pos.y);
-    pos.x -= canvasSpace.offsetLeft;
-    pos.y -= canvasSpace.offsetTop;
+ 	if (paint){
+ 		this.style.backgroundColor = colorChoice;
+ 	}
 }
 
 function finishDrawing(){
