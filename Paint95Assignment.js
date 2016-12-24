6d8fcc81b0fb00b32f058634f70fd3f92e9df8aa
edenadler <eden.n.adler@gmail.com>
2016-12-24 21:28:32 +0200

    changed images, still working on toggling color picker

--- Paint95Assignment.js
+++ Paint95Assignment.js
@@ -19,7 +19,7 @@ for (var i = 0; i<buttonArray.length; i++){
 	menuOptions.setAttribute("class","menuChoices");
 	menuDiv.appendChild(menuOptions);
 
-	document.getElementsByClassName("menuChoices")[i].setAttribute("style","height:50px; width: 50px; cursor: pointer; position: relative; display: block; margin: 5px; border-radius: 4px; border: 2px solid white; background-color: white;");
+	document.getElementsByClassName("menuChoices")[i].setAttribute("style","height:60px; width: 60px; cursor: pointer; position: relative; display: block; margin: 5px; background-repeat: no-repeat; padding-top: 25px; ");
 	document.getElementsByClassName("menuChoices")[i].style.backgroundImage = "url('./images/"+backgroundImages[i]+".png')";
 	document.getElementsByClassName("menuChoices")[i].style.backgroundSize = "contain";
 }
@@ -28,23 +28,27 @@ var popupToggle = document.createElement("DIV");
 popupToggle.setAttribute("id","popupColor");
 document.getElementById("colorPalatte").appendChild(popupToggle);
 var popupDiv = document.getElementById("popupColor").style.display;
-popupDiv = "none";
-document.getElementById("colorPalatte").onclick = changeVisibility;
+var show = false;
+document.getElementById("colorPalatte").addEventListener("click", changeVisibility);
 
 
 function changeVisibility(){
-	if (popupDiv == "none"){
+	if (show == false){
 		popupDiv = "block";
+		show = true;
 	}
-	else if (popupDiv == "block"){
+	else if (show = true){
 		popupDiv = "none";
+		show = false;
 	}
 }
 var colorInput = ["R","G","B"];
 
 var colorForm = document.createElement("FORM");
 document.getElementById("popupColor").appendChild(colorForm);
-var formLabel = document.createElement("LABEL");
+var formLabel = document.createElement("P");
+formLabel.style.marginBottom  = "10px";
+formLabel.style.marginTop = "5px";
 colorForm.appendChild(formLabel);
 var node = document.createTextNode("Choose your RGB color:");
 formLabel.appendChild(node);
@@ -55,6 +59,7 @@ for (var i = 0; i<colorInput.length; i++){
 	inputNum.setAttribute("type","number");
 	inputNum.setAttribute("min","0");
 	inputNum.setAttribute("max","255");
+	inputNum.setAttribute("placeholder","0");
 	inputNum.setAttribute("class","RGB");
 	colorForm.appendChild(inputNum);
 
@@ -64,16 +69,6 @@ document.getElementsByClassName("RGB")[i].addEventListener("click", showBackgrou
 var showRGB = document.createElement("DIV");
 	showRGB.setAttribute("id", "showColor");
 	colorForm.appendChild(showRGB);
-
-// var rgbValue = [0,0,0];
-// for (var i = 0; i<rgbValue.length; i++){
-// 	localStorage.setItem("rInput",JSON.stringify(document.getElementById("R").value));
-// }
-// 	var r = JSON.parse(localStorage.getItem(rgbValue[0]));
-// 	var g = JSON.parse(localStorage.getItem(rgbValue[1]));
-// 	var b = JSON.parse(localStorage.getItem(rgbValue[2]));
-// 	var colorRGB = r,g,b;
-// 	document.getElementById("showColor").style.backgroundColor = "rgb("+colorRGB+")";
 
 var allColors = document.getElementsByClassName("colors");
 	for (var i = 0; i<allColors.length; i++){
@@ -159,4 +154,5 @@ function showBackgroundColor(){
 	var b = document.getElementById("B").value;
 
 	document.getElementById("showColor").style.backgroundColor = "rgb("+r+","+g+","+b+")";
+	colorChoice = "rgb("+r+","+g+","+b+")";
 }
